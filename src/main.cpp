9c4d3b0752d254f4b8d45d2ab287d9a9e40dfee5
MFdesigns <michel.faeh@stafag.ch>
2020-10-03 17:00:36 +0200

    Added basic command line argument validation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <filesystem>
 
-int main() {
-    std::cout << "Hello World!\n";
+void printCLIUsage() {
+    std::cout << "usage: uvm <path>\n";
+}
+
+int main(int argc, char* argv[]) {
+    // Check if minimal CLI arguments are provided
+    if (argc < 2) {
+        printCLIUsage();
+        return -1;
+    }
+
+    // Check if target UX file exists
+    char* sourcePath = argv[1];
+    std::filesystem::path p{sourcePath};
+    if (!std::filesystem::exists(p)) {
+        std::cout << "Target file '" << p.string() << "' does not exist\n";
+        return -1;
+    }
 }
